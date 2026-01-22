d541a3b2f5afa52a640389de1e1be1c669ae3781
Wei Tang <tangwei@smail.nju.edu.cn>
2026-01-22 18:22:19 +0100

    rewrite find_subspace_matrixrep; simplify find_solution

--- src/find_subspace.jl
+++ src/find_subspace.jl
@@ -25,55 +25,6 @@ function matrix_for_linear_function(T::AbstractTensorMap, f_op::Function; _mappi
 end
 
 """
-    find_subspace_matrixrep(T::AbstractTensorMap, P_init::Matrix{<:Number}, f_op::Function, D::AbstractMatrix;
-        tol=1e-8, _mapping_table=mapping_table(T))
-
-Project `P_init` onto the subspace transforming as matrix representation `D` under `f_op`.
-Returns a basis matrix for the resulting subspace.
-
-Notes:
-- `P_init` columns are an initial basis in parameter space.
-- Solves `(I ⊗ M_op) vec(X) - (D^T ⊗ I) vec(X) = 0 ` by looking for the null space of the LHS
-- The result is a (not necessarily orthonormal) basis in the original parameter
-  space.
-"""
-function find_subspace_matrixrep(T::AbstractTensorMap, P_init::Matrix{<:Number}, f_op::Function, D::AbstractMatrix;
-    tol::Real=1e-8, _mapping_table::MappingTable=mapping_table(T))
-
-    init_subspace_size = size(P_init, 2)
-    if init_subspace_size == 0
-        println("input subspace is empty")
-        return P_init
-    end
-    if size(D, 1) != size(D, 2)
-        throw(ArgumentError("representation matrix must be square"))
-    end
-
-    M_op = matrix_for_linear_function(T, f_op; _mapping_table=_mapping_table)
-
-    k = size(P_init, 2)
-    d = size(D, 1)
-    Tprom = promote_type(eltype(M_op), eltype(D))
-    D = Matrix{Tprom}(D)
-    I_d = Matrix{Tprom}(I, d, d)
-    MP = Matrix{Tprom}(M_op * P_init)
-    Pp = Matrix{Tprom}(P_init)
-    K = kron(I_d, MP) - kron(transpose(D), Pp)
-    _, S, V = svd(K)
-    idx = findall(s -> s <= tol, S)
-    if isempty(idx)
-        return P_init[:, 1:0]
-    end
-
-    cols = Matrix{Tprom}(undef, size(P_init, 1), 0)
-    for j in idx
-        X = reshape(V[:, j], k, d)
-        cols = hcat(cols, P_init * X)
-    end
-    return cols
-end
-
-"""
     find_subspace(T::AbstractTensorMap, P_init::Matrix{<:Number}, f_op::Function; λ=1.0, is_hermitian=false, tol=1e-8, _mapping_table=mapping_table(T))
 
 Project an initial subspace `P_init` onto the eigenspace of `f_op` with eigenvalue `λ`.
@@ -94,12 +45,6 @@ function find_subspace(T::AbstractTensorMap, P_init::Matrix{<:Number}, f_op::Fun
     end
 
     if norm(P_init' * P_init - Matrix{eltype(P_init)}(I, init_subspace_size, init_subspace_size)) > tol
-        #for ix in 1:init_subspace_size
-        #    for iy in 1:init_subspace_size
-        #        print(" ", (P_init' * P_init)[ix, iy], " ")
-        #    end
-        #    println(" ")
-        #end
         error("input subspace is not orthonormal")
     end
 
@@ -125,6 +70,68 @@ function find_subspace(T::AbstractTensorMap, P_init::Matrix{<:Number}, f_op::Fun
 end
 
 """
+    find_subspace_matrixrep(T::AbstractTensorMap, P_init::Matrix{<:Number}, f_op::Function, D::AbstractMatrix;
+        tol=1e-8, _mapping_table=mapping_table(T))
+
+FIXME. docstring for this function is outdated. need to be updated!
+
+Project `P_init` onto the subspace transforming as matrix representation `D` under `f_op`.
+Returns a basis matrix for the resulting subspace.
+
+Notes:
+- `P_init` columns are an initial basis in parameter space.
+- Solves `(I ⊗ M_op) vec(X) - (D^T ⊗ I) vec(X) = 0 ` by looking for the null space of the LHS
+- The result is a (not necessarily orthonormal) basis in the original parameter
+  space.
+"""
+function find_subspace_matrixrep(T::AbstractTensorMap, P_init::Matrix{<:Number}, f_op::Function, D::AbstractMatrix;
+    tol::Real=1e-8, _mapping_table::MappingTable=mapping_table(T))
+
+    init_subspace_size = size(P_init, 2)
+    d_irrep = size(D, 1)
+    num_sol_init = size(P_init, 2)
+    num_paras = num_free_parameters(T; _mapping_table=_mapping_table)
+
+    if init_subspace_size == 0
+        println("input subspace is empty")
+        return P_init
+    end
+    if num_paras != size(P_init, 1) * d_irrep
+        throw(ArgumentError("P_init should have $(num_paras)*$(d_irrep) rows"))
+    end
+    if size(D, 1) != size(D, 2)
+        throw(ArgumentError("representation matrix must be square"))
+    end
+
+    M_op = matrix_for_linear_function(T, f_op; _mapping_table=_mapping_table)
+
+    Tprom = promote_type(eltype(M_op), eltype(D))
+    D = Matrix{Tprom}(D)
+    I_paras = Matrix{Tprom}(I, num_paras, num_paras)
+
+    K = zeros(Tprom, num_paras*d_irrep, num_sol_init*d_irrep)
+    for ix in 1:d_irrep
+        indices_L = (ix-1)*num_paras+(1:num_paras)
+        indices_R = (ix-1)*num_sol_init+(1:num_sol_init)
+        K[indices_L, indices_R] = P_init[indices_L, :]
+    end
+    K .= - kron(transpose(D), I_paras) * K
+    for ix in 1:d_irrep
+        indices_L = (ix-1)*num_paras+(1:num_paras)
+        indices_R = (ix-1)*num_sol_init+(1:num_sol_init)
+        K[indices_L, indices_R] .+= M_op * P_init[indices_L, :]
+    end
+    
+    _, S, V = svd(K)
+    idx = findall(s -> s <= tol, S)
+    if isempty(idx)
+        return P_init[:, 1:0]
+    end
+
+    return V[:, idx]
+end
+
+"""
     find_solution(T::AbstractTensorMap, P_sol::Matrix{<:Number}; _mapping_table=mapping_table(T))
 
 Convert a solution subspace `P_sol` into normalized symmetric tensor solutions.
@@ -139,20 +146,8 @@ function find_solution(T::AbstractTensorMap, P_sol::Matrix{<:Number}; _mapping_t
     if num_solutions == 0
         return Vector{typeof(T)}(undef, 0)
     end
-    # Orthonormalize columns in order (modified Gram-Schmidt) to preserve block structure.
-    max_col_norm = maximum(norm(P_sol[:, ix]) for ix in 1:num_solutions)
-    tol = max_col_norm * sqrt(eps(real(eltype(P_sol))))
-    P_ortho = Matrix{eltype(P_sol)}(undef, size(P_sol, 1), 0)
-    for ix in 1:num_solutions
-        v = copy(P_sol[:, ix])
-        for jx in 1:size(P_ortho, 2)
-            v -= (P_ortho[:, jx]' * v) * P_ortho[:, jx]
-        end
-        nv = norm(v)
-        if nv > tol
-            P_ortho = hcat(P_ortho, v / nv)
-        end
-    end
+    F = qr(P_sol)
+    P_ortho = Matrix(F.Q)
     if size(P_ortho, 2) == 0
         return Vector{typeof(T)}(undef, 0)
     end
