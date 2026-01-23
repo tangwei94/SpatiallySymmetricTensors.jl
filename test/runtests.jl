8ac581a579a2e87614e18f81f215b2ac5364eaf4
Wei Tang <tangwei@smail.nju.edu.cn>
2026-01-23 13:01:24 +0100

    tests for defined pointgroup

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,7 @@ using TensorKit
 using SpatiallySymmetricTensors
 
 include("test_spatial_operations.jl");
+include("test_defined_pointgroup.jl");
 include("test_pointgroup.jl");
 include("test_square_lattice_SU2.jl");
 include("test_u1_charge_conjugation.jl");
