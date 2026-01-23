9bcef593ec5a5abac6820312e833aa2a3b24761c
Wei Tang <tangwei@smail.nju.edu.cn>
2026-01-23 12:32:59 +0100

    projector for C6v D2

--- src/D2.jl
+++ src/D2.jl
@@ -6,14 +6,23 @@ const D2_ops = Dict{Symbol, D2_perm_type}(
     :Id => ((1, ), (2, 3, 4, 5)),
     :σv1 => ((1, ), (4, 3, 2, 5)),
     :σv2 => ((1, ), (2, 5, 4, 3)),
+    :C2 => ((1, ), (4, 5, 2, 3)),
 )
 const D2_A_reps = Dict{Symbol, Int}(
-    :Id => 1, :σv1 => 1, :σv2 => 1,
+    :Id => 1, :σv1 => 1, :σv2 => 1, :C2 => 1,
 )
 const D2_B1_reps = Dict{Symbol, Int}(
-    :Id => 1, :σv1 => -1, :σv2 => -1,
+    :Id => 1, :σv1 => -1, :σv2 => -1, :C2 => 1,
+)
+const D2_B2_reps = Dict{Symbol, Int}(
+    :Id => 1, :σv1 => 1, :σv2 => -1, :C2 => -1,
+)
+const D2_B3_reps = Dict{Symbol, Int}(
+    :Id => 1, :σv1 => -1, :σv2 => 1, :C2 => -1,
 )
 
 group_elements(::D2) = D2_ops
 irrep_chars(::D2, ::Val{:A}) = D2_A_reps
 irrep_chars(::D2, ::Val{:B1}) = D2_B1_reps
+irrep_chars(::D2, ::Val{:B2}) = D2_B2_reps
+irrep_chars(::D2, ::Val{:B3}) = D2_B3_reps
--- test/test_pointgroup.jl
+++ test/test_pointgroup.jl
@@ -50,3 +50,55 @@ end
         end
     end
 end
+
+@testset "projector_function C6v, SU2Space" begin
+    V = SU2Space(1//2=>1, 0=>1)
+    P = SU2Space(1//2=>1)
+
+    for (group, irrep_name) in [
+        (C6v(), :A1), (C6v(), :A2), (C6v(), :B1), (C6v(), :B2),
+    ]
+        fproj = SpatiallySymmetricTensors.projector_function(group, irrep_name)
+
+        T = rand(ComplexF64, P, V^6)
+        Tp = fproj(T)
+
+        # projector should be idempotent
+        @test norm(fproj(Tp) - Tp) < 1e-10
+
+        # projected tensor should transform under the target irrep
+        ops = SpatiallySymmetricTensors.group_elements(group)
+        reps = SpatiallySymmetricTensors.irrep_chars(group, irrep_name)
+        for (name, perm) in ops
+            χ = get(reps, name, nothing)
+            @test χ !== nothing
+            @test norm(permute(Tp, perm) - χ * Tp) < 1e-10
+        end
+    end
+end
+
+@testset "projector_function D2, SU2Space" begin
+    V = SU2Space(1//2=>1, 0=>1)
+    P = SU2Space(1//2=>1)
+
+    for (group, irrep_name) in [
+        (D2(), :A), (D2(), :B1), (D2(), :B2), (D2(), :B3),
+    ]
+        fproj = SpatiallySymmetricTensors.projector_function(group, irrep_name)
+
+        T = rand(ComplexF64, P, V^4)
+        Tp = fproj(T)
+
+        # projector should be idempotent
+        @test norm(fproj(Tp) - Tp) < 1e-10
+
+        # projected tensor should transform under the target irrep
+        ops = SpatiallySymmetricTensors.group_elements(group)
+        reps = SpatiallySymmetricTensors.irrep_chars(group, irrep_name)
+        for (name, perm) in ops
+            χ = get(reps, name, nothing)
+            @test χ !== nothing
+            @test norm(permute(Tp, perm) - χ * Tp) < 1e-10
+        end
+    end
+end
