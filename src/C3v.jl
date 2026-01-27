495ca09d5cc58d437b14ea54a913ac73f8cc1f5e
Wei Tang <tangwei@smail.nju.edu.cn>
2026-01-27 13:03:32 +0100

    data for C3v E

--- src/C3v.jl
+++ src/C3v.jl
@@ -21,9 +21,24 @@ const C3v_A2_reps = Dict{Symbol, Int}(
     :R1 => 1, :R2 => 1,
     :σv1 => -1, :σv2 => -1, :σv3 => -1,
 )
+const C3v_E_reps = Dict{Symbol, Int}(
+    :Id => 2,
+    :R1 => -1, :R2 => -1,
+    :σv1 => 0, :σv2 => 0, :σv3 => 0,
+)
+const C3v_E_rep = Dict{Symbol, Matrix{ComplexF64}}(
+    :Id => ComplexF64[1 0; 0 1],
+    :R1 => ComplexF64[-1/2 -sqrt(3)/2; sqrt(3)/2 -1/2],
+    :R2 => ComplexF64[-1/2 sqrt(3)/2; -sqrt(3)/2 -1/2],
+    :σv1 => ComplexF64[1 0; 0 -1],
+    :σv2 => ComplexF64[-1/2 -sqrt(3)/2; -sqrt(3)/2 1/2],
+    :σv3 => ComplexF64[-1/2 sqrt(3)/2; sqrt(3)/2 1/2],
+)
 
 group_elements(::C3v) = C3v_ops
 irrep_chars(::C3v, ::Val{:A1}) = C3v_A1_reps
 irrep_chars(::C3v, ::Val{:A2}) = C3v_A2_reps
+irrep_chars(::C3v, ::Val{:E}) = Dict(name => tr(mat) for (name, mat) in C3v_E_rep)
 irrep_rep(::C3v, ::Val{:A1}) = Dict(name => [χ;;] for (name, χ) in C3v_A1_reps)
 irrep_rep(::C3v, ::Val{:A2}) = Dict(name => [χ;;] for (name, χ) in C3v_A2_reps)
+irrep_rep(::C3v, ::Val{:E}) = C3v_E_rep
