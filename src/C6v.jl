3226ae7239127fc61109ae8bbfebb0051b0dcce3
Wei Tang <tangwei@smail.nju.edu.cn>
2026-01-27 13:10:11 +0100

    C6v E irrep

--- src/C6v.jl
+++ src/C6v.jl
@@ -41,13 +41,45 @@ const C6v_B2_reps = Dict{Symbol, Int}(
     :σv1 => -1, :σv2 => -1, :σv3 => -1,
     :R1 => -1, :R2 => 1, :C2 => -1, :R4 => 1, :R5 => -1,
 )
+const C6v_E1_rep = Dict{Symbol, Matrix{ComplexF64}}(
+    :Id => ComplexF64[1 0; 0 1],
+    :R1 => ComplexF64[1/2 -sqrt(3)/2; sqrt(3)/2 1/2],
+    :R2 => ComplexF64[-1/2 -sqrt(3)/2; sqrt(3)/2 -1/2],
+    :C2 => ComplexF64[-1 0; 0 -1],
+    :R4 => ComplexF64[-1/2 sqrt(3)/2; -sqrt(3)/2 -1/2],
+    :R5 => ComplexF64[1/2 sqrt(3)/2; -sqrt(3)/2 1/2],
+    :σv1 => ComplexF64[1 0; 0 -1],
+    :σv2 => ComplexF64[-1/2 sqrt(3)/2; sqrt(3)/2 1/2],
+    :σv3 => ComplexF64[-1/2 -sqrt(3)/2; -sqrt(3)/2 1/2],
+    :σd1 => ComplexF64[1/2 sqrt(3)/2; sqrt(3)/2 -1/2],
+    :σd2 => ComplexF64[-1 0; 0 1],
+    :σd3 => ComplexF64[1/2 -sqrt(3)/2; -sqrt(3)/2 -1/2],
+)
+const C6v_E2_rep = Dict{Symbol, Matrix{ComplexF64}}(
+    :Id => ComplexF64[1 0; 0 1],
+    :R1 => ComplexF64[-1/2 -sqrt(3)/2; sqrt(3)/2 -1/2],
+    :R2 => ComplexF64[-1/2 sqrt(3)/2; -sqrt(3)/2 -1/2],
+    :C2 => ComplexF64[1 0; 0 1],
+    :R4 => ComplexF64[-1/2 -sqrt(3)/2; sqrt(3)/2 -1/2],
+    :R5 => ComplexF64[-1/2 sqrt(3)/2; -sqrt(3)/2 -1/2],
+    :σv1 => ComplexF64[1 0; 0 -1],
+    :σv2 => ComplexF64[-1/2 -sqrt(3)/2; -sqrt(3)/2 1/2],
+    :σv3 => ComplexF64[-1/2 sqrt(3)/2; sqrt(3)/2 1/2],
+    :σd1 => ComplexF64[-1/2 sqrt(3)/2; sqrt(3)/2 1/2],
+    :σd2 => ComplexF64[1 0; 0 -1],
+    :σd3 => ComplexF64[-1/2 -sqrt(3)/2; -sqrt(3)/2 1/2],
+)
 
 group_elements(::C6v) = C6v_ops
 irrep_chars(::C6v, ::Val{:A1}) = C6v_A1_reps
 irrep_chars(::C6v, ::Val{:A2}) = C6v_A2_reps
 irrep_chars(::C6v, ::Val{:B1}) = C6v_B1_reps
 irrep_chars(::C6v, ::Val{:B2}) = C6v_B2_reps
+irrep_chars(::C6v, ::Val{:E1}) = Dict(name => tr(mat) for (name, mat) in C6v_E1_rep)
+irrep_chars(::C6v, ::Val{:E2}) = Dict(name => tr(mat) for (name, mat) in C6v_E2_rep)
 irrep_rep(::C6v, ::Val{:A1}) = Dict(name => [χ;;] for (name, χ) in C6v_A1_reps)
 irrep_rep(::C6v, ::Val{:A2}) = Dict(name => [χ;;] for (name, χ) in C6v_A2_reps)
 irrep_rep(::C6v, ::Val{:B1}) = Dict(name => [χ;;] for (name, χ) in C6v_B1_reps)
 irrep_rep(::C6v, ::Val{:B2}) = Dict(name => [χ;;] for (name, χ) in C6v_B2_reps)
+irrep_rep(::C6v, ::Val{:E1}) = C6v_E1_rep
+irrep_rep(::C6v, ::Val{:E2}) = C6v_E2_rep
