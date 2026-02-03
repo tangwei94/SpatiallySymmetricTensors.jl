8c48fc8bd1ab5ac2617d57e0ce8d5d1b5d48ef4f
Wei Tang <tangwei@smail.nju.edu.cn>
2026-02-03 16:16:22 +0100

    remove krylovkit depdencence

--- src/SpatiallySymmetricTensors.jl
+++ src/SpatiallySymmetricTensors.jl
@@ -4,10 +4,7 @@ __precompile__(true)
 
 using LinearAlgebra
 using TensorKit
-using KrylovKit
-
 export mapping_table, num_free_parameters, set_data_by_vector!, set_data_by_vector, selector, matrix_for_spatial_operation
-export mpo_ovlp, mpotensor_dag
 export matrix_for_linear_function, find_subspace
 export AbstractPointGroup, find_solution
 export C3v, C4v, C6v, C4, D2
@@ -23,7 +20,6 @@ include("C4v.jl");
 include("C6v.jl");
 include("C4.jl");
 include("D2.jl");
-include("square_lattice_SU2.jl");
 include("u1_charge_conjugation.jl");
 
 
